f47cb287d7c9a61a485fb8c0d2ac5070c2723878
Aadarshag7 <aadarshgautam40@gmail.com>
2026-01-22 23:16:28 +0545

    continue

--- resources/views/Sect/edit.blade.php
+++ resources/views/Sect/edit.blade.php
@@ -6,8 +6,18 @@
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
-<body class="h-screen bg-red
-">
+<body class="h-screen bg-red-200">
+<div class="h-full bg-blue-200">
+    <form action="{{}}" method="POST">
+        @method('PUT')
+        @csrf 
+        <label for="name">name</label>
+        <input type="text" name="name" id="name" value="{{$sect->name}}" placeholder="NAME">
+        <label for="date">Date</label>
+        <input type="text" name="date" id="date" vlaue="{{$sect->date}}" placeholder="DATE">
+        <button type="submit">DONE</button>
+    </form>
+</div>
 
     
 </body>
